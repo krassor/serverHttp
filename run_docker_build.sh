92ecacec907a359a61ac1284e80fa6752aed0e0a
krassor <krassor@mail.ru>
2022-12-01 09:35:54 +0300

    gitignore

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -1,10 +1,10 @@
 #go build -o bin/app-serverHttp app/main.go./
 echo "stopping app-server-http-news container"
-docker stop app-server-http-news
+docker stop app-server-http-news-v1.1
 echo "removing app-server-http-news container"
-docker rm app-server-http-news
+docker rm app-server-http-news-v1.1
 echo "removing app-server-http-news image"
-docker rm app-server-http-news
+docker rmi app-server-http-news
 echo "building new app-server-http-news image"
 docker build -t app-server-http-news .
 docker run -it -d --publish 8001:8001 --name app-server-http-news-v1.1 app-server-http-news
